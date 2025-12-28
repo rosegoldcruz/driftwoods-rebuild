36635cd93b2391e17b8d852eb3da65deb668b6e1
Rosegoldcruz <vulpine444@gmail.com>
2025-12-27 21:26:39 -0700

    Remove menu carousel (FoodGallery) from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import { Hero, AboutSection, FoodGallery, NewsletterForm, TrustSection } from '@/components'
+import { Hero, AboutSection, NewsletterForm, TrustSection } from '@/components'
 
 export default function HomePage() {
   return (
     <>
       <Hero />
       <TrustSection />
-      <FoodGallery />
+
       <AboutSection />
       <NewsletterForm />
     </>
